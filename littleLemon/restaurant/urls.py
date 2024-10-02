4d73b074503dc0fb83d48f0fec854f6eebf677f3
Adam1618 <mohamed.adam.jemal@gmail.com>
2024-10-02 13:09:15 +0100

    adjusting the app files

--- littleLemon/restaurant/urls.py
+++ littleLemon/restaurant/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('menu-items/', views.MenuItemsView.as_view(), name="menu-list"),  # Changed name for clarity
-    path('menu-items/<int:pk>/', views.SingleMenuItemView.as_view(), name="menu-detail"),  # Added trailing slash
+    path('menu-items/', views.MenuItemsView.as_view(), name="menu-list"),  
+    path('menu-items/<int:pk>/', views.SingleMenuItemView.as_view(), name="menu-detail"),  
 ]
